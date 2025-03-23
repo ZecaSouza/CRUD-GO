2601f4c543fe4ffb434c22f9a60f6df3d4693897
Zeca Souza <szeca00@gmail.com>
2025-03-23 19:34:19 -0300

    Corrigido fluxo de criação de usuários e validações: - Ajustada validação de erros no método CreatdUser. - Adicionada validação personalizada para campos incorretos no formulário. - Melhorado o retorno de erros com mensagens mais claras.

--- src/controller/creaserUser.go
+++ src/controller/creaserUser.go
@@ -2,15 +2,33 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ZecaSouza/CRUD-GO/src/configuration/rest_err"
 	"github.com/ZecaSouza/CRUD-GO/src/configuration/rest_err/model/request"
+	"github.com/ZecaSouza/CRUD-GO/src/configuration/rest_err/model/response"
+	"github.com/ZecaSouza/CRUD-GO/src/configuration/rest_err/validation"
 	"github.com/gin-gonic/gin"
 )
 
 func CreatdUser(c *gin.Context) {
-
+	log.Println("Create User controller")
 	var userRequest request.UserRequest
+
+	if error := c.ShouldBindJSON(&userRequest); error != nil {
+		log.Println("Error trying to marshal object error: ", error)
+		errRest := validation.ValidateUserError(error)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	fmt.Println(userRequest)
+	c.JSON(200, response.UserResponse{
+		ID: 	"teste",
+		Name: userRequest.Name,
+		Email: userRequest.Email,
+	})
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil{
 		restErr := rest_err.NewBadRequestError(
